215ce35d160265e72dd92feb7ecb7d3cca913763
LÊ MINH KHÔI <khoi.kioto@gmail.com>
2025-12-17 21:07:21 +0700

    done to move docker

--- src/main/java/com/practice/service/api/RoleController.java
+++ src/main/java/com/practice/service/api/RoleController.java
@@ -1,5 +1,6 @@
 package com.practice.service.api;
 
+import com.practice.service.dto.RoleResponse;
 import com.practice.service.entities.auth.Role;
 import com.practice.service.repositories.RoleRepository;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +21,18 @@ public class RoleController {
 
     @GetMapping
     public ResponseEntity<?> getRoles() {
-        List<Role> roles = roleRepository.findAll();
+        List<RoleResponse> roles = roleRepository.findAll()
+                .stream()
+                .map(role -> {
+                    // map Role entity -> RoleResponse DTO
+                    RoleResponse dto = new RoleResponse();
+                    dto.setId(role.getId().toString());
+                    dto.setCode(role.getName());
+                    dto.setDescription(role.getDescription());
+                    return dto;
+                })
+                .toList();
+
         return ResponseEntity.ok(roles);
     }
 }
---
+++ src/main/java/com/practice/service/dto/RoleResponse.java
@@ -0,0 +1,10 @@
+package com.practice.service.dto;
+
+import lombok.Data;
+
+@Data
+public class RoleResponse {
+    private String id;
+    private String code;
+    private String description;
+}
