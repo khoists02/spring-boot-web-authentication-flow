24c3e6ed49421db450a6905e88dcb0942c09dd7a
LÊ MINH KHÔI <khoi.kioto@gmail.com>
2025-12-18 16:36:07 +0700

    shaking

--- src/main/java/com/practice/service/entities/auth/User.java
+++ src/main/java/com/practice/service/entities/auth/User.java
@@ -1,18 +1,21 @@
+/*
+ * FuckUB Pty. Ltd. ("LKG") CONFIDENTIAL
+ * Copyright (c) 2025 FuckUB project Pty. Ltd. All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains the property of LKG. The intellectual and technical concepts contained
+ * herein are proprietary to LKG and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material is strictly forbidden unless prior written permission is obtained
+ * from LKG.  Access to the source code contained herein is hereby forbidden to anyone except current LKG employees, managers or contractors who have executed
+ * Confidentiality and Non-disclosure agreements explicitly covering such access.
+ */
 package com.practice.service.entities.auth;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.practice.service.entities.BaseEntity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-
-import java.util.Collection;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @Entity
 @Table(name = "users")
